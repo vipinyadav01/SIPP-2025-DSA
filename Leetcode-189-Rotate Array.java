3e524eb57c8164cd40ca7a0316b22b042dbb942f
vipinyadav01 <vipinyadav9m@gmail.com>
2025-06-16 09:48:30 +0530

    Add solution for LeetCode Problem 189: Rotate Array

---
+++ Leetcode-189-Rotate Array.java
@@ -0,0 +1,21 @@
+//Given an integer array nums, rotate the array to the right by k steps, where k is non-negative.
+
+
+class Solution {
+    public void rotate(int[] nums, int k) {
+        k = k % nums.length;
+                reverse(nums, 0, nums.length - 1);
+                reverse(nums, 0, k - 1);
+                reverse(nums, k, nums.length - 1);
+    }
+    
+    private void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+                        start++;
+            end--;
+        }
+    }
+}
